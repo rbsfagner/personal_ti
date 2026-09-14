42887f29b3691c1e55c1a10335a296bcd074824d
robsonfagner <rbs.fagner@gmail.com>
2026-09-13 22:11:25 -0300

    Tentando finalizar curso de Pandas

--- TMY/Python/pandas/curso/dia03/01_filtro.py
+++ TMY/Python/pandas/curso/dia03/01_filtro.py
@@ -1,6 +1,34 @@
-#%%
-
+# %%
 import pandas as pd
 
-df = pd.read_csv("../data/transacoes.csv")
+df = pd.read_csv(r"C:\Users\rbsfa\Documents\Git\personal_ti\TMY\Python\pandas\curso\data\transacoes.csv",sep=";")
 df.head()
+# %%
+pontos = [ 10,1,1,1,50,100,130,30,25,50]
+filtro = []
+
+valores_50 = []
+for i in pontos:
+    filtro.append(i>=50)
+
+
+resultado = []
+for i in range(len(pontos)):
+    if filtro[i]:
+        resultado.append(pontos[i]) 
+        
+resultado   
+# %%
+brinquedo = pd.DataFrame(
+    {
+        "nome": ["teo", "nah", "mah"],
+        "idade": [32,35,14],
+        "uf": ["sp", "pr", "rj"],     
+     }
+)
+
+brinquedo["idade"] >= 18
+
+# %%
+
+parei no 12:50
